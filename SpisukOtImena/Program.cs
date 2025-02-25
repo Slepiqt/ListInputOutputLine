05cfdc0bcafb70f4cf5259f800a74911aab2f843
Slepiqt <atanasbakalov27@gmail.com>
2025-02-25 21:34:38 +0200

    thirth commit

---
+++ SpisukOtImena/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SpisukOtImena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedi imena: ");
+            List<string> names = Console.ReadLine().Split(' ').ToList();
+            for (int i = names.Count-1; i >= 0; i--)
+            {
+                Console.Write($"{names[i]} ; " );
+            }
+        }
+        
+    }
+}
---
+++ VuvejdaneNaSpisuk1Red/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace VuvejdaneNaSpisuk1Red
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int i = 0;i < nums.Count; i++)
+            {
+                Console.WriteLine($"nums[{i}] = {nums[i]}");
+            }
+        }
+    }
+}
